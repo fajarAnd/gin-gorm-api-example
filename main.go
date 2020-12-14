2695396d9368638865db0fcb72ae3fdd7bd4e296
fajarandriansyah <fajarandriansyah1988@gmail.com>
2020-12-14 17:48:10 +0700

    using mysql

--- main.go
+++ main.go
@@ -1,38 +1,93 @@
 package main
 
+import "C"
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
+var db *gorm.DB
+var err error
+
 type Person struct {
 	ID        uint   `json:"id"`
 	FirstName string `json:"firstname"`
-	LastName  string `json:lastname`
+	LastName  string `json:"lastname"`
+	City      string `json:"city"`
 }
 
 func main() {
-	db, _ := gorm.Open("sqlite3", "./gorm.db")
+	db, err = gorm.Open("mysql","root:admin123@tcp(127.0.0.1:3306)/first?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		fmt.Println(err)
+	}
 	defer db.Close()
 
 	db.AutoMigrate(&Person{})
 
-	p1 := Person{
-		FirstName: "Jhon",
-		LastName:  "Doe",
+	r := gin.Default()
+	r.GET("/people", GetProjects)
+	r.GET("/people/:id", GetPerson)
+	r.POST("/people", CreatePerson)
+	r.PUT("/people/:id", UpdatePerson)
+	r.DELETE("/people/:id", DeletePerson)
+
+	r.Run(":8080")
+}
+
+func DeletePerson(c *gin.Context) {
+	var person Person
+	id := c.Params.ByName("id")
+
+	d := db.Where("id = ?", id).Delete(&person)
+	fmt.Println(d)
+	c.JSON(200, gin.H{"id #" + id: "deleted"})
+}
+
+func UpdatePerson(c *gin.Context) {
+	var person Person
+	id := c.Params.ByName("id")
+
+	if err := db.Where("id = ?", id).First(&person).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
 	}
-	p2 := Person{
-		FirstName: "Jane",
-		LastName: "Smith",
+
+	c.BindJSON(&person)
+
+	db.Save(&person)
+	c.JSON(200, person)
+}
+
+func CreatePerson(c *gin.Context) {
+	var person Person
+	c.BindJSON(&person)
+
+	db.Create(&person)
+	c.JSON(200, person)
+}
+
+func GetPerson(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var person Person
+
+	if err := db.Where("id = ?", id).First(&person).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.JSON(200, person)
 	}
+}
 
-	db.Create(&p1)
-	var p3 Person // identify a Person type for us to store the results in
-	db.First(&p3)
-
-	fmt.Println(p1.FirstName)
-	fmt.Println(p2.LastName)
-	fmt.Println(p3.LastName)
-
+func GetProjects(c *gin.Context) {
+	var people []Person
+	if err := db.Find(&people).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.JSON(200, people)
+	}
 }
